36fdb7ba24c8376d33cf44c48a31efbe48c5e925
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-07-20 10:52:53 +0530

    Add get method

---
+++ Microservice.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,55 @@
+﻿using Microservice.CouponAPI.DATA;
+using Microservice.CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+        public CouponAPIController(AppDBContext context)
+        {
+            _context = context;
+                
+        }
+
+        [HttpGet]
+        public object get()
+        {
+            try
+            {
+
+                IEnumerable<Coupon> objlist = _context.CouponDS.ToList();
+                return objlist;
+
+            }catch(Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return null;
+
+        }
+
+        [HttpGet("{id}")]
+        public object getbyid(int id)
+        {
+            try
+            {
+                Coupon objlist = _context.CouponDS.First(u => u.CouponID == id);
+                return objlist;
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return null;
+        }
+
+
+    }
+}
