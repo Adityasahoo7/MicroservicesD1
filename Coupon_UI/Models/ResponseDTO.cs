5d295d1c819950e70956ab2cdad984ee9c861f85
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-07-21 11:22:28 +0530

    Add ui project

---
+++ Coupon_UI/Models/ResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Coupon_UI.Models
+{
+    public class ResponseDTO
+    {
+        public object? Result { get; set; }
+        public bool isSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+
+
+    }
+}
