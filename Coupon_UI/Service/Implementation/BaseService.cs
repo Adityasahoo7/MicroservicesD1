95def15430a715584d2778dcb47ebf944cce346d
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-07-21 12:07:11 +0530

    Add Service v2

--- Coupon_UI/Service/Implementation/BaseService.cs
+++ Coupon_UI/Service/Implementation/BaseService.cs
@@ -1,6 +1,19 @@
-﻿namespace Coupon_UI.Service.Implementation
+﻿using Coupon_UI.Models;
+using Coupon_UI.Service.Interface;
+using Microservice.CouponAPI.Models.DTO;
+
+namespace Coupon_UI.Service.Implementation
 {
-    public class BaseService
+    public class BaseService : IBaseService
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory factory)
+        {
+            _httpClientFactory = factory;
+        }
+        public async Task<ResponseDTO?> SendAsync(RequestDTO dto)
+        {
+            
+        }
     }
 }
